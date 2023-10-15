ce951f7cf11b8bf741262b3fb13e50e921e1644f
Omofomah-david-joseph <omofomahdavidjoseph365@gmail.com>
2023-10-15 14:22:40 -0700

    prints number in hexa-decimal.

---
+++ print_hexa_deci.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * print_hexa_deci - Prints an unsigned number in hexadecimal notation
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_hexa_deci(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789abcdef", buffer,
+		flags, 'x', width, precision, size));
+}
