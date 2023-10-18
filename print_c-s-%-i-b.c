a6fe591707815ab903e8481955f2f16142190aa0
Emmanuel Onyeozu <104470064+eminovaden@users.noreply.github.com>
2023-10-18 03:22:31 +0100

    Update print_c-s-%-i-b.c

--- print_c-s-%-i-b.c
+++ print_c-s-%-i-b.c
@@ -13,11 +13,11 @@
  * Return: Number of chars printed
  */
 int print_c(va_list args, char lim[],
-	      int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-	      char c = va_arg(args, int);
+	char c = va_arg(args, int);
 
-	      return (write_c(c, lim, flags, width, precision, size));
+	return (write_c(c, lim, flags, width, precision, size));
 }
 /************************* PRINT A STRING *************************/
 /**
@@ -31,45 +31,45 @@ int print_c(va_list args, char lim[],
  * Return: Number of chars printed
  */
 int print_s(va_list args, char lim[],
-	      int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-	      int length = 0, i;
-	      char *str = va_arg(args, char *);
+	int length = 0, i;
+	char *str = va_arg(args, char *);
 
-      	NO(lim);
-	      NO(flags);
-	      NO(width);
-	      NO(precision);
-	      NO(size);
-	      if (str == NULL)
-	      {
-		            str = "(null)";
-		            if (precision >= 6)
-			                  str = "      ";
-	      }
+	NO(lim);
+	NO(flags);
+	NO(width);
+	NO(precision);
+	NO(size);
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
 
-	      while (str[length] != '\0')
-		      length++;
+	while (str[length] != '\0')
+		length++;
 
-	      if (precision >= 0 && precision < length)
-		      length = precision;
+	if (precision >= 0 && precision < length)
+		length = precision;
 
-	      if (width > length)
-	      {
-		      if (flags & C_MINUS)
-		      {
-			      write(1, &str[0], length);
-			      for (i = width - length; i > 0; i--)
-			              write(1, " ", 1);
-			      return (width);
-		       }
-		       else
-		       {
-			      for (i = width - length; i > 0; i--)
-				      write(1, " ", 1);
-			      write(1, &str[0], length);
-			      return (width);
-		       }
+	if (width > length)
+	{
+		if (flags & C_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
 	}
 
 	return (write(1, str, length));
